c9e16f4cb78405f161bb3014100d9aadef84f715
Steven Robert <stevenrobert08@gmail.com>
2024-09-28 21:22:50 +0200

    remove mvc + fetch all articles

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,43 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Mes articles</title>
 </head>
+
 <body>
-    <?php require './view/header.php'; ?>
+    <div class="container">
+        <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Accueil</a></li>
+            </ul>
+        </header>
+    </div>
 </body>
+
 </html>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=basic-crud", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$datas = $conn->query("SELECT * FROM articles")->fetchAll();
+
+foreach ($datas as $data) {
+    echo 
+        '<div class="card mb-3">
+            <div class="card-header">Featured</div>
+        <div class="card-body">
+            <h5 class="card-title"> ' . $data['name'] . ' </h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>';
+}
+
+?>
--- view/header.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <header class="d-flex justify-content-center py-3">
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Accueil</a></li>
-      </ul>
-    </header>
-  </div>
